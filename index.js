ac9d120f30c635c74e315f7cb54b4b5d0490ac7d
NotEvenCloseBaby <robert.krzysztof.luty@gmail.com>
2019-05-20 12:58:23 +0200

    hgf

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const mysql = require('mysql');
 const http = require('http');
 const PORT = process.env.PORT || 5000;
 
+const db = mysql.createConnection(process.env.JAWSDB_URL);
+
+db.connect();
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.write('Hello World!\n');
-
-  const db = mysql.createConnection(process.env.JAWSDB_URL);
-
-  db.connect();
 
   db.query('SELECT id, ip FROM IP WHERE id=1', function(err, rows, fields) {
     if (err) throw err;
@@ -17,10 +17,10 @@ const server = http.createServer((req, res) => {
     res.write(rows[0].ip);
   });
 
-  db.end();
-
   res.end(); //end the response
 });
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
+
+  db.end();
