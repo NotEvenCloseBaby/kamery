58417038e79a4cb291b0a6db5a72365fc04e4882
NotEvenCloseBaby <robert.krzysztof.luty@gmail.com>
2019-05-20 11:49:42 +0200

    fd

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
+console.log('Hello World!');
