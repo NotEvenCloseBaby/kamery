4f29221ad6d58eafe2b778ccb323eed3ee09df5f
NotEvenCloseBaby <robert.krzysztof.luty@gmail.com>
2019-05-20 14:17:34 +0200

    fgh

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function handle_database(req,res) {
             return res.json({'error': true, 'message': 'Error occurred'+err});
         }
                 //connection will be released as well.
-                res.redirect(rows[0].ip);
+                res.redirect("http://" + rows[0].ip + "/");
        });
 }
 
