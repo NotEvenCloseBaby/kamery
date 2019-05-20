54c70ec5b7982eca07070f29cd84be7abd665bab
NotEvenCloseBaby <robert.krzysztof.luty@gmail.com>
2019-05-20 13:16:10 +0200

    gfd

--- index.js
+++ index.js
@@ -3,19 +3,18 @@ const http = require('http');
 const PORT = process.env.PORT || 5000;
 
 const db = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
+db.connect();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.write('Hello World!\n');
 
-  db.connect();
   db.query('SELECT id, ip FROM IP WHERE id=1', function(err, rows, fields) {
     if (err) throw err;
 
     res.write(rows[0].ip);
   });
-  db.end();
 
   res.end(); //end the response
 });
