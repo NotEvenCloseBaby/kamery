0df2312c28671800f204f7344882b383e4e438a8
NotEvenCloseBaby <robert.krzysztof.luty@gmail.com>
2019-05-20 12:04:07 +0200

    gfdg

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}/`);
+});
